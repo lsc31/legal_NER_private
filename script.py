bea8d7b84c39ee935d7f6ae6494c720652238bc2
Lakshmishree Chandra <lakshmishree.c@gmail.com>
2022-12-11 18:16:46 -0700

    Changes to script.py for extracting entities from doc

--- script.py
+++ script.py
@@ -2,13 +2,15 @@ import spacy
 from data_preparation import get_text_from_indiankanoon_url
 from spacy import displacy
 from legal_ner import extract_entities_from_judgment_text
+import urllib
 
 legal_nlp=spacy.load('en_legal_ner_trf')
 print("calling url")
-judgment_text = get_text_from_indiankanoon_url('https://indiankanoon.org/doc/542271/')
+judgment_text = urllib.request.urlopen('https://raw.githubusercontent.com/OpenNyAI/Opennyai/master/samples/sample_judgment1.txt').read().decode()
 
 print(judgment_text)
 preamble_spiltting_nlp = spacy.load('en_core_web_sm')
+#run_type='sent'
 run_type='doc' ###  trade off between accuracy and runtime
 do_postprocess=True 
 combined_doc = extract_entities_from_judgment_text(judgment_text,legal_nlp,preamble_spiltting_nlp,run_type,do_postprocess)
